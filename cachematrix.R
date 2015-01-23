c5bf9ca939b297fd53f5851da1120530f8fae7e5
Renato <rpceqb@hotmail.com>
2015-01-23 20:16:37 +0000

    Checked spelling and added an example on how to use the functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,11 @@
-## These functions allows the user to compute the inverse of
-## a matrix, a time-consuming process, especially if used in
-## a loop. In this function we cache the computed values 
+## These functions allow the user to compute the inverse of 
+## a matrix, a time-consuming process, especially if used 
+## in a loop. In this function we cache the computed values 
 ## instead of them being recomputed, saving up time.
 
-## In this function we cache the inverse of the matrix by
-## assigning a value to an object in an environment that is
-## different from the current environment.
+## In this function we cache the inverse of the matrix by 
+## assigning a value to an object in an environment that is 
+## different from the currentenvironment.
 
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL #Creates a null matrix
@@ -14,8 +14,8 @@ makeCacheMatrix <- function(x = matrix()) {
                 inv <<- NULL #Assign the null matrix to an object in a different environment
         }
         get <- function() x #get the value of the matrix
-        setinv <- function(solve) inv <<- solve #set the value of the inverse
-        getinv <- function () inv #get the value of the inverse
+        setinv <- function(solve) inv <<- solve #set the value of the inverse of the matrix
+        getinv <- function () inv #get the value of the inverse of the matrix
         list(set = set, get = get, setinv = setinv,
              getinv = getinv) #creates a list containing the cached values
 }
@@ -33,5 +33,12 @@ cacheSolve <- function(x, ...) {
         data <- x$get() #gets the matrix stored in the other function
         inv <- solve(data, ...) #Calculates the inverse of the matrix
         x$setinv(inv) #stores the inverse of the matrix in the cache
-        inv #returns the inverse of the matris
+        inv #returns the inverse of the matrix
 }
+
+
+## To run, first store a matrix in the cache using makeCacheMatrix() function, then run the stored matrix in cacheSolve().
+## Example
+## >m <- matrix(1:4, 2, 2)
+## >store <- makeCacheMatrix(m)
+## >cacheSolve(store)
